f4647c67559f045ca8a45450b7a2e1e63f7f67c6
dizzzha <159957473+dizzzha@users.noreply.github.com>
2024-08-09 00:22:37 +0300

    Update views.py

--- articles_app/views.py
+++ articles_app/views.py
@@ -3,17 +3,21 @@ from django.http import JsonResponse
 import json
 from .text import dictionary
 
+
 def index(request):
     return render(request, 'index.html')
 
+
 def get_value_for_key(key):
     return dictionary.get(key, "Значение не найдено")
+
 
 def get_value(request):
     if request.method == 'POST':
         keys = json.loads(request.POST.get('keys'))
         values = [get_value_for_key(key) for key in keys]
         return JsonResponse({'values': values})
+
 
 def get_keys(request):
     keys = list(dictionary.keys())
