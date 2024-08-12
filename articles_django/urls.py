4355c360d818169d0f679a76977fb2f7c740c132
dizzzha <159957473+dizzzha@users.noreply.github.com>
2024-08-13 00:15:57 +0300

    Update urls.py

--- articles_django/urls.py
+++ articles_django/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('get_value/', views.get_value, name='get_value'),
     path('get_keys/', views.get_keys, name='get_keys'),
+    path('get_values/', views.get_values, name='get_values'),
+    path('search/', views.search, name='search'),
 ]
