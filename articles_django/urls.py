54f268069a8b4321aad413122ec3cc5cb7625225
dizzzha <159957473+dizzzha@users.noreply.github.com>
2024-08-23 20:14:11 +0300

    Update urls.py
    add 2 floor

--- articles_django/urls.py
+++ articles_django/urls.py
@@ -3,10 +3,12 @@ from articles_app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('second_floor/', views.second_floor, name='second_floor'),
     path('get_value/', views.get_value, name='get_value'),
     path('get_keys/', views.get_keys, name='get_keys'),
+    path('get_keys2/', views.get_keys2, name='get_keys2'),
     path('get_values/', views.get_values, name='get_values'),
+    path('get_values2/', views.get_values2, name='get_values2'),
     path('search/', views.search, name='search'),
-    path('search_image/', views.search_image, name='search_image'),
-    path('set_floor/', views.set_floor, name='set_floor'),
+    path('search2/', views.search2, name='search'),
 ]
